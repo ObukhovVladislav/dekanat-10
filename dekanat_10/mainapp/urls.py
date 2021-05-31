490fc26969d4e36c0fe3d19d48cd84c13b31f97b
ObukhovVladislav <vobukhov10@gmail.com>
2021-05-31 11:43:18 +0500

    refactoring

--- dekanat_10/mainapp/urls.py
+++ dekanat_10/mainapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', mainapp.index, name='index'),
     path('groups/', mainapp.groups, name='groups'),
     path('group/student/<int:group_pk/', mainapp.students_page, name='students_page'),
+    path('group/create', mainapp.create_group, name='create_group')
 ]
